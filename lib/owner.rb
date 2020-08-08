8cd6916e52fc08da88a5b8edf1350e225d9ce8a2
Pablo Menz <p.menz.r@gmail.com>
2020-08-08 20:55:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
 class Owner
+
+@@all=[]
+def self.count 
+count
+end
+
+def self.reset_all
+  @@all.clear 
+end 
+
+def self.all 
+  @@all 
+end 
+
+  
+def name 
+end
+
+def species
+end
+
+def say_species 
+end
   # code goes here
 end
